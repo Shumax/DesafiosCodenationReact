7a39ef949de5413e7317f6396a0ae7a8f3ba4fdf
Shumax <maxcastro.mv@gmail.com>
2020-06-04 20:47:47 -0300

    Dashboard

---
+++ react-17/src/routes/DashboardRoute.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Switch, useRouteMatch,} from 'react-router-dom';
+
+import {
+  getCategoriesFailed,
+  getCategoriesRequest,
+  getCategoriesSuccess,
+  getUserFailed,
+  getUserRequest,
+  getUserSuccess,
+  logout,
+} from '../actions';
+
+import { endpoints } from '../modules/endpoints';
+import { request } from '../modules/request';
+import { WelcomeBox } from '../components';
+import {
+  Categories,
+  Dashboard,
+  PrivateRoute,
+  Topbar,
+} from '../containers';
+
+
+const { getCategories, getUserProfile,  } = endpoints;
+
+const DashboardRoute = () => {
+  const { auth, content, user } = useSelector(state => state);
+  const { path, url } = useRouteMatch();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const requestOptions = {
+      ...getUserProfile.options,
+      headers: { 'Authorization': `Bearer ${auth.accessToken}` }
+    }
+
+    dispatch(getUserRequest());
+
+    request(getUserProfile.url, requestOptions)
+      .then(data => dispatch(getUserSuccess(data)))
+      .catch(error => {
+        if (error === 401) {
+          dispatch(logout());
+
+          return;
+        }
+
+        dispatch(getUserFailed(error));
+      });
+
+  }, [auth, dispatch]);
+
+  useEffect(() => {
+    const requestOptions = {
+      ...getCategories.options,
+      headers: { 'Authorization': `Bearer ${auth.accessToken}` }
+    }
+
+    dispatch(getCategoriesRequest());
+
+    request(getCategories.url, requestOptions)
+      .then(data => dispatch(getCategoriesSuccess(data)))
+      .catch(error => {
+        if (error === 401) {
+          dispatch(logout());
+
+          return;
+        }
+
+        dispatch(getCategoriesFailed(error))
+      });
+
+  }, [auth, dispatch]);
+
+  return (
+    <Dashboard>
+      <Topbar />
+
+      <Switch>
+        
+      </Switch>
+    </Dashboard>
+  );
+}
+
+export default DashboardRoute;
