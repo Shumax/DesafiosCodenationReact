910d2de7b566df3f5f3c332135504c2b5eee0f7d
Shumax <maxcastro.mv@gmail.com>
2020-06-25 18:26:28 -0300

    Action fetch Api

--- fashionista/src/services/api.js
+++ fashionista/src/services/api.js
@@ -1,4 +1,11 @@
+import axios from "axios";
 
+const baseURL = "https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog";
+
+export default async function api(){
+  return await axios.get(baseURL);
+}
+/*
 export default async function fetchAPI(){
     await fetch('https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog')
       
@@ -7,7 +14,7 @@ export default async function fetchAPI(){
         throw erro;
       });
 }
-/*import axios from  "axios";
+import axios from  "axios";
 
 const api = axios.create({
     baseURL: 'https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog'
