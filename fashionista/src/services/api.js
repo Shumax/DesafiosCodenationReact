f04bbb51a9679b2aab724b03b390cf178feca1b3
Shumax <maxcastro.mv@gmail.com>
2020-06-25 19:53:42 -0300

    Action fetchAPI

--- fashionista/src/services/api.js
+++ fashionista/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL = "https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog";
 
-export default async function api(){
+export default async function fetchAPI(){
   return await axios.get(baseURL);
 }
 /*
