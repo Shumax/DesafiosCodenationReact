903a029bdb5bd04fa0cd87c4d5539c0d3dffa7e7
Shumax <maxcastro.mv@gmail.com>
2020-10-29 17:49:14 -0300

    Start server

--- nodejs-12/src/server.js
+++ nodejs-12/src/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const routes = require('./routes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./routes.js');
+
+const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.use(routes);
-app.listen(3000);
+app.listen(port);
 
 module.exports = app;
